f43a884961f7df06b109144111218035bdf84f19
Sir-MetalSnake <ruizruizkhrisna@gmail.com>
2023-10-08 17:06:15 -0700

    se arreglo un problema en la busqueda del dato

--- MyTables/tipos_de_piñatas.py
+++ MyTables/tipos_de_piñatas.py
@@ -3,7 +3,7 @@ from database import *
 
 class tipos_de_piñatas(Model):
     idTipos_de_piñatas = IntegerField(primary_key=True)
-    Nombre_festividad = CharField(max_length=45)
+    Tipo = CharField(max_length=45)
 
     def __int__(self):
         return self.idTipos_de_piñatas
