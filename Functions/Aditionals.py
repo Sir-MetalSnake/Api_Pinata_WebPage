1442f0b718f3fa4d13055bffeb63b77ec44479b9
Sir-MetalSnake <ruizruizkhrisna@gmail.com>
2023-10-28 13:38:07 -0700

    Se agrego el metodo de comparación en el correo

--- Functions/Aditionals.py
+++ Functions/Aditionals.py
@@ -6,17 +6,21 @@ import hashlib
 import string
 from fastapi import HTTPException
 
+char = (string.digits + string.
+        ascii_letters + string.punctuation)
+
 from MyTables.secret_key import *
+
 
 class Sender:
     Email_sender = ''
     sender_password = ''
 
-    def __init__(self,Email_sender,sender_password):
+    def __init__(self, Email_sender, sender_password):
         self.Email_sender = Email_sender
         self.sender_password = sender_password
 
-    def Send_Mail(self,subject,receiver,file, code):
+    def Send_Mail(self, subject, receiver, file, code):
         Myfile = open(file, "r", encoding="utf-8")
         readFile = Myfile.read()
         readFile = readFile.replace("MI_CODE", code)
@@ -29,35 +33,29 @@ class Sender:
             mail.login(self.Email_sender, self.sender_password)
             mail.sendmail(self.Email_sender, receiver, msg.as_string())
 
-async def SendMAil_User(user):
+
+async def Send_Mail_Code_Verify(user):
     cli = usuario_cliente.select().where(usuario_cliente.usuario == user)
+    sender = Sender("ansirkhziur@gmail.com", "diyv behc uqqm tydq")  # en esta parte llamamos a la clase Sender
+    # Se crea de forma automatica el codigo para conectarse
+    p1 = ''.join(random.choice(char) for i in range(3))
+    p2 = ''.join(random.choice(char) for i in range(3))
+    p3 = ''.join(random.choice(char) for i in range(3))
+    code = p1 + '-' + p2 + '-' + p3  # o63-948-eu3
+    sender.Send_Mail(subject="Cambio de Contraseña", receiver=cli.Correo, file="res/Recovery_pass.html",
+                     code=code)  # Hacemos llamado a la funcion
+    h1 = hashlib.sha512(code.encode()).hexdigest()
+    t = datetime.utcnow() + timedelta(minutes=3)
+    print(t)
+    secret_key.create(Key_name=h1, Id_usuario=cli.idusuarios, Expire=t)
+    return "Email Send Sucessfully"
 
 
-
-# diyv behc uqqm tydq
-# async def Verification_Email():
-#     return True
-#
-#
-# class emailSend:
-#     sender = ""
-#     password = ""
-#
-#     def __init__(self, sender, password):
-#         self.sender = sender
-#         self.password = password
-
-    # def sendM(self, subject, receiver, file, code):
-    #     Myfile = open('res/Recovery_pass.html', "r", encoding="utf-8")
-    #     readFile = Myfile.read()
-    #     readFile = readFile.replace("MI_CODE", '2932011')
-    #     readFile = readFile.replace("MI_CORREO", correo)
-    #     msg = MIMEText(readFile, 'html')
-    #     msg['Subject'] = subject
-    #     msg['From'] = self.sender
-    #     msg['To'] = self.password
-    #     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as mail:
-    #         mail.login(self.sender, self.password)
-    #         mail.sendmail(self.sender, receiver, msg.as_string())
-    #
-    #     return "Email Send Sucessfully"
+async def Compare_Secret_Key(Key):
+    loki = datetime.utcnow()
+    cypher = hashlib.sha512(Key.encode()).hexdigest()
+    valhalla = secret_key.select().where(secret_key.KeyName == cypher and loki <= secret_key.Expire)
+    if valhalla:
+        return True
+    else:
+        raise HTTPException(404, 'El código no coincide o Ya Expiro')
