0efe3160a3ff090f70774bbeb7b8be1e4c9e67cc
Sir-MetalSnake <ruizruizkhrisna@gmail.com>
2023-10-28 19:36:01 -0700

    Se agregaron 2 tablas

--- MyTables/colors.py
+++ MyTables/colors.py
@@ -1,1 +1,14 @@
 from database import *
+from MyTables.piñata import piñata
+class colors(Model):
+    id_color = IntegerField(primary_key=True)
+    Name = CharField(50)
+    Imagen = CharField(50)
+    Id_Piñatas = IntegerField(ForeignKeyField(piñata, field="idPiñatas", column_name="Id_Piñatas"))
+
+    def __str__(self):
+        return self.Name
+
+    class Meta:
+        database = database
+        table_name = 'colors'
