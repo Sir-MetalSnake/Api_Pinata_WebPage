784724224419964c46238e003342553363465432
Sir-MetalSnake <ruizruizkhrisna@gmail.com>
2023-11-01 10:55:16 -0700

    Bitacora de Snake. Hemos mejorado la conexion de la BD con exito jajaja

--- MyTables/usuario_cliente.py
+++ MyTables/usuario_cliente.py
@@ -2,7 +2,7 @@ from database import *
 class usuario_cliente(Model):
     idusuarios = IntegerField(primary_key=True)
     usuario = CharField(max_length=45)
-    contraseña = CharField(max_length=45)
+    contraseña = TextField(primary_key=False)
     Correo = CharField(max_length=45)
     def __str__(self):
         return self.usuario
--- database.py
+++ database.py
@@ -1,9 +1,15 @@
+import os
 from peewee import *
-
+from dotenv import load_dotenv
 # oap2
-
+load_dotenv()
+DB_NAME = str(os.getenv('DB_NAME'))
+USER = str(os.getenv('DB_USER'))
+PASSWD = str(os.getenv('DB_PASSW'))
+HOST = str(os.getenv('DB_HOST'))
+PORT = int(os.getenv('DB_PORT'))
 database = MySQLDatabase(
-    'pinatas_fantasy',
-    user='root', password='Ruiz181002',
-    host='localhost', port=3306
+    DB_NAME,
+    user=USER, password=PASSWD,
+    host=HOST, port=PORT
 )
