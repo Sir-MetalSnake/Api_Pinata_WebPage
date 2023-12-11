e1f935cc22bf695c0658d89b7d97624fe95e6aa8
Sir-MetalSnake <ruizruizkhrisna@gmail.com>
2023-12-11 12:32:12 -0700

    Bitacora Espacial he mejorado la api en un 100%.

--- database.py
+++ database.py
@@ -3,13 +3,9 @@ from peewee import *
 from dotenv import load_dotenv
 # oap2
 load_dotenv()
-DB_NAME = str(os.getenv('DB_NAME'))
-USER = str(os.getenv('DB_USER'))
-PASSWD = str(os.getenv('DB_PASSW'))
-HOST = str(os.getenv('DB_HOST'))
-PORT = int(os.getenv('DB_PORT'))
+
 database = MySQLDatabase(
-    DB_NAME,
-    user=USER, password=PASSWD,
-    host=HOST, port=PORT
+    'pinatas_fantasy',
+    user='root', password='EAb562bDEBfAGAgAHG3dHB32fdcCH6B4',
+    host='roundhouse.proxy.rlwy.net', port=40116
 )
